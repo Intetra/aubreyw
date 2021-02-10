17dde8262817fa9803fae68aa08e8f43b76557a8
aubrey <intetra@gmail.com>
2021-02-10 13:56:12 -0700

    cleaned up the code a little

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,30 +1,7 @@
-import React, { useState, useLayoutEffect } from "react";
-import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+import useWindowSize from "../customHooks/useWindowSize"
 
-//start paste================================================
-function useWindowSize() {
-  const [size, setSize] = useState([0, 0]);
-  useLayoutEffect(
-    () => {
-      function updateSize() {
-        setSize([window.innerWidth, window.innerHeight]);
-      }
-      window.addEventListener("resize", updateSize);
-      updateSize();
-      return () => window.removeEventListener("resize", updateSize);
-    },[]);
-  return size;
-}
-
-function ShowWindowDimensions(props) {
-  const [width, height] = useWindowSize();
-  return (
-    <span>
-      Window size: {width} x {height}
-    </span>
-  );
-}
-//finish paste============================================
 
 const IndexScreen = () => {
   const { container, background, moon } = styles;
