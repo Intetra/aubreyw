17f0d0965886750bbfcd26843e116926669da862
aubrey <intetra@gmail.com>
2021-02-18 11:24:53 -0700

    got the conditional rendering working on android and browser

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,27 +1,60 @@
 import React, { useEffect } from "react";
 import { View, StyleSheet, Text, ActivityIndicator, Animated, Image } from "react-native";
 import useWindowSize from "../customHooks/useWindowSize";
-import spinningImage from "../customHooks/spinningImage"
+import spinningImage from "../customHooks/spinningImage";
+import Images from '../images/index'
+
+
 
 const Template = (device, spin) => {
-  const { moon, container, sky, ground } = styles
-  console.log(device)
-  console.log(spin)
-  const moonSource = require(`../images/${device}/moon.png`)
+  const { moon, moonHolder, container, sky, skyHolder, ground, groundHolder } = styles
+  const { bigGround, bigMoon, bigSky, desktopGround, desktopMoon, desktopSky,
+    ipadGround, ipadMoon, ipadSky, phoneGround, phoneMoon, phoneSky } = Images
+
+  let groundSource
+  let moonSource
+  let skySource
+
+  if (device === "phone") {
+    groundSource = phoneGround
+    moonSource = phoneMoon
+    skySource = phoneSky
+  } else if (device === "ipad") {
+    groundSource = ipadGround
+    moonSource = ipadMoon
+    skySource = ipadSky
+  } else if (device === "desktop") {
+    groundSource = desktopGround
+    moonSource = desktopMoon
+    skySource = desktopSky
+  } else if (device === "4k") {
+    groundSource = bigGround
+    moonSource = bigMoon
+    skySource = bigSky
+  } else {
+    return device
+  }
+
   return (
     <View style={container}>
-      <Animated.Image
-        source={moonSource}
-        style={[moon, {transform: [{rotate: spin}]}]}
-      />
-      <Image
-        source={require(`../images/${device}/sky.png`)}
-        style={sky}
-      />
-      <Image
-        source={require(`../images/${device}/ground.png`)}
-        style={ground}
-      />
+      <View style={groundHolder}>
+        <Image
+          source={groundSource}
+          style={ground}
+        />
+      </View>
+      <Animated.View style={[moonHolder, {transform: [{rotate: spin}]}]}>
+        <Image
+          source={moonSource}
+          style={moon}
+        />
+      </Animated.View>
+      <View style={skyHolder}>
+        <Image
+          source={skySource}
+          style={sky}
+        />
+      </View>
     </View>
   );
 };
@@ -31,7 +64,6 @@ const Background = () => {
 
   const { container, moon, test } = styles;
   const [width, height] = useWindowSize();
-  console.log(width);
 
   if (width == 0) {
     return <ActivityIndicator />;
@@ -54,37 +86,69 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "center"
+  },
+  skyHolder: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    zIndex: 0,
+    elevation: 0
   },
   sky: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover"
+  },
+  groundHolder: {
     position: "absolute",
     width: "100%",
     height: "100%",
-    resizeMode: "cover",
-    zIndex: 1,
+    zIndex: 2,
+    elevation: 2
   },
   ground: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover"
+  },
+  moonHolder: {
     position: "absolute",
     width: "100%",
     height: "100%",
-    resizeMode: "cover",
-    zIndex: 3,
+    zIndex: 1,
+    elevation: 1
   },
   moon: {
-    position: "absolute",
     width: "100%",
     height: "100%",
-    resizeMode: "contain",
-    zIndex: 2,
+    resizeMode: 'contain'
   },
   test: {
     backgroundColor: "gray",
     position: "absolute",
     color: "red",
     width: "100%",
-    height: "100%",
-    zIndex: 2,
+    height: "100%"
   },
 });
 
 export default Background;
+
+
+/*
+<View style={container}>
+  <Animated.Image
+    source={phoneMoonSource}
+    style={[moon, {transform: [{rotate: spin}]}]}
+  />
+  <Image
+    source={sky}
+    style={sky}
+  />
+  <Image
+    source={require(`../images/${device}/ground.png`)}
+    style={ground}
+  />
+</View>
+*/
---
+++ src/images/index.js
@@ -0,0 +1,16 @@
+const images = {
+  bigGround: require('../images/4k/ground.png'),
+  bigMoon: require('../images/4k/moon.png'),
+  bigSky: require('../images/4k/sky.png'),
+  desktopGround: require('../images/desktop/ground.png'),
+  desktopMoon: require('../images/desktop/moon.png'),
+  desktopSky: require('../images/desktop/sky.png'),
+  ipadGround: require('../images/ipad/ground.png'),
+  ipadMoon: require('../images/ipad/moon.png'),
+  ipadSky: require('../images/ipad/sky.png'),
+  phoneGround: require('../images/phone/ground.png'),
+  phoneMoon: require('../images/phone/moon.png'),
+  phoneSky: require('../images/phone/sky.png')
+}
+
+export default images
